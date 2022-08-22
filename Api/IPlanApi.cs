a35fbf3a6ef894a1bc8e0b493ec11115e9dd5025
Marcus Schweda <ms@gschwind-software.de>
2022-08-22 23:03:20 +0200

    Endpunkte Status Quo

--- Api/IPlanApi.cs
+++ Api/IPlanApi.cs
@@ -1,3 +1,4 @@
+using Gschwind.Lighthouse.Example.Models.Data;
 using Gschwind.Lighthouse.Example.Models.Plans;
 using Refit;
 
@@ -17,5 +18,39 @@ public interface IPlanApi {
     /// <returns>Eine Aufgabe, die die Antwort der Abfrage enthält</returns>
     [Get("/plans/{planId}")]
     Task<ApiResponse<Plan>> GetAsync(int planId);
+
+    /// <summary>
+    /// Vorgänge des Status Quo abfragen
+    /// </summary>
+    /// <param name="clientId">Die vom System vergebene Id des Kunden</param>
+    /// <returns>Eine Aufgabe, die die Antwort der Abfrage enthält</returns>
+    [Get("/plans/byClient/{clientId}/statusQuo/data")]
+    Task<ApiResponse<IReadOnlyCollection<PlanData>>> GetStatusQuoDataAsync(Guid clientId);
+
+    /// <summary>
+    /// Vorgänge des Status Quo abfragen
+    /// </summary>
+    /// <param name="clientNumber">Die benutzerdefinierte Kundennummer des Kunden</param>
+    /// <returns>Eine Aufgabe, die die Antwort der Abfrage enthält</returns>
+    [Get("/plans/byClient/no:{clientNumber}/statusQuo/data")]
+    Task<ApiResponse<IReadOnlyCollection<PlanData>>> GetStatusQuoDataAsync(string clientNumber);
+
+    /// <summary>
+    /// Vorgänge des Status Quo aktualisieren
+    /// </summary>
+    /// <param name="clientId">Die vom System vergebene Id des Kunden</param>
+    /// <param name="data">Die neuen Vorgangsdaten</param>
+    /// <returns>Eine Aufgabe, die die Antwort der Abfrage enthält</returns>
+    [Get("/plans/byClient/{clientId}/statusQuo/data")]
+    Task<IApiResponse> UpdateStatusQuoDataAsync(Guid clientId, IEnumerable<PlanData> data);
+
+    /// <summary>
+    /// Vorgänge des Status Quo aktualisieren
+    /// </summary>
+    /// <param name="clientNumber">Die benutzerdefinierte Kundennummer des Kunden</param>
+    /// <param name="data">Die neuen Vorgangsdaten</param>
+    /// <returns>Eine Aufgabe, die die Antwort der Abfrage enthält</returns>
+    [Get("/plans/byClient/no:{clientNumber}/statusQuo/data")]
+    Task<IApiResponse> UpdateStatusQuoDataAsync(string clientNumber, IEnumerable<PlanData> data);
 
 }
