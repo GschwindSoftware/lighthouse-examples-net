0255941fce30f1643886f5eeec1eeae858ebf8ae
Marcus Schweda <ms@gschwind-software.de>
2023-11-13 15:58:49 +0100

    fix: Hierarchie Betriebskosten

--- Models/Data/OperatingCost.cs
+++ Models/Data/OperatingCost.cs
@@ -3,5 +3,5 @@ namespace Gschwind.Lighthouse.Example.Models.Data;
 /// <summary>
 /// Betriebskosten
 /// </summary>
-public record OperatingCost : PlanData {
+public record OperatingCost : CashflowSubFinance {
 }
